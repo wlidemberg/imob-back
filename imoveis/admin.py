873f81a65b0f9e34ee90082cbe97ad262a73a221
Wlidemberg <automatizei@gmail.com>
2025-06-24 17:47:07 -0300

    criação do modelo Imovel e registro no admin

--- imoveis/admin.py
+++ imoveis/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Imovel
 
 # Register your models here.
+
+@admin.register(Imovel)
+class ImovelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'tipo', 'preco', 'disponivel', 'data_cadastro')
+    list_filter = ('tipo', 'disponivel')
+    search_fields = ('titulo', 'endereco')
+    ordering = ('-data_cadastro',)
--- imoveis/models.py
+++ imoveis/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Modelo Imovel
+class Imovel(models.Model):
+    # Campos básicos do imóvel
+    titulo = models.CharField(
+        max_length=100,
+        help_text='Título ou nome do imóvel(Ex: Casa em Paraty)'
+    )
+
+    descricao = models.TextField(
+        blank=True,
+        help_text='Descrição detalhada do imóvel'
+    )
+
+    endereco = models.CharField(
+        max_length=200,
+        help_text='Endereço completo do imóvel'
+    )
+
+    # Definindo tipos de imóves
+    tipo_imovel = [
+        ('AP', 'Apartamento'),
+        ('CA', 'Casa'),
+        ('SA', 'Sala'),
+        ('LJ', 'Loja'),
+        ('SBJ', 'Sobreloja'),
+        ('GP', 'Galpão'),
+        ('TE', 'Terreno'),
+        ('PT', 'Parte'), # quando é somente parte de um imovel
+    ]
+
+    tipo = models.CharField(
+        max_length=3,
+        choices=tipo_imovel,
+        default='CA',
+        help_text='Tipo do Imóvel'
+    )
+
+    preco = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        help_text='Valor do imóvel (Ex.: 1000000,00).'
+    )
+
+    disponivel = models.BooleanField(
+        default=True,
+        help_text='Se está disponível para venda ou locação.'
+    )
+
+    data_cadastro = models.DateTimeField(
+        auto_now_add=True,
+        help_text='Data em que o imóvel foi cadastrado.'
+    )
+
+    data_atualizacao = models.DateTimeField(
+        auto_now=True,
+        help_text='Data da ultima atualização.'
+    )
+
+    # Representação do objeto no admin e nos logs
+    def __str__(self):
+        return f'{self.titulo} - {self.get_tipo_display()}'
